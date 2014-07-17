f62af6dc10f5f4265dd2869768b8ea03fa406979
vvakame <vvakame+dev@gmail.com>
2014-07-17 23:34:35 +0900

    review-ext.rbのMarkdown互換具合を向上

--- review-ext.rb
+++ review-ext.rb
@@ -37,5 +37,79 @@ module ReVIEW
       end
       puts "```"
     end
+
+    def table(lines, id = nil, caption = nil)
+      rows = []
+      sepidx = nil
+      lines.each_with_index do |line, idx|
+        if /\A[\=\-]{12}/ =~ line
+          # just ignore
+          #error "too many table separator" if sepidx
+          sepidx ||= idx
+          next
+        end
+        rows.push line.strip.split(/\t+/).map {|s| s.sub(/\A\./, '') }
+      end
+      rows = adjust_n_cols(rows)
+
+      begin
+        table_header id, caption unless caption.nil?
+      rescue KeyError
+        error "no such table: #{id}"
+      end
+      table_begin rows.first.size
+      return if rows.empty?
+      if sepidx
+        sepidx.times do
+          tr rows.shift.map {|s| th(s) }
+        end
+        table_border rows.first.size
+        rows.each do |cols|
+          tr cols.map {|s| td(s) }
+        end
+      else
+        rows.each do |cols|
+          h, *cs = *cols
+          tr [th(h)] + cs.map {|s| td(s) }
+        end
+      end
+      table_end
+    end
+
+    def table_header(id, caption)
+      if get_chap.nil?
+        puts %Q[#{I18n.t("table")}#{I18n.t("format_number_header_without_chapter", [@chapter.table(id).number])}#{I18n.t("caption_prefix")}#{compile_inline(caption)}]
+      else
+        puts %Q[#{I18n.t("table")}#{I18n.t("format_number_header", [get_chap, @chapter.table(id).number])}#{I18n.t("caption_prefix")}#{compile_inline(caption)}]
+      end
+      blank
+    end
+
+    def table_begin(ncols)
+    end
+
+    def tr(rows)
+      puts "|#{rows.join("|")}|"
+    end
+
+    def table_border(ncols)
+      puts (0..ncols).map{"|"}.join(":--")
+    end
+
+    def th(str)
+      "#{str}"
+    end
+
+    def td(str)
+      "#{str}"
+    end
+
+    def table_end
+      blank
+    end
+
+    def inline_br(str)
+      "\n"
+    end
   end
 end
