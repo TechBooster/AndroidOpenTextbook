1dc65c77a480800cc54f81ba16fd0d56220715dd
Toshihiro Yagi <sylc.yagi@gmail.com>
2014-09-24 00:27:31 +0900

    Add sample code for 17.2.

---
+++ code/library/ProfileApp/src/com/example/profileapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.profileapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
